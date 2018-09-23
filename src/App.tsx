ad891c1aafb99dd6263ee08b7ac261fd701076c5
Kadhir <kadhirvelm@gmail.com>
2018-09-23 00:36:18 -0700

    Add splash page (#75)

--- src/App.tsx
+++ src/App.tsx
@@ -1,93 +1,18 @@
 import * as React from "react";
-import { connect } from "react-redux";
-import { bindActionCreators, Dispatch } from "redux";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import { Spinner, Toaster } from "@blueprintjs/core";
+import { AtlasP } from "./AtlasP";
+import { SplashPage } from "./Components/Splash/Splash";
 
-import { EmptyDatabaseCache } from "./State/DatabaseActions";
-import IStoreState from "./State/IStoreState";
-import { setToast } from "./Utils/Toaster";
-
-import "./App.css";
-
-export interface IAppStoreProps {
-  fetching: boolean;
-  isLoggedIn: boolean;
-}
-
-export interface IAppDispatchProps {
-  emptyCache(): void;
-}
-
-export interface IAppState {
-  Element: React.ComponentClass;
-  hasErrored: boolean;
-}
-
-class PureApp extends React.PureComponent<IAppStoreProps & IAppDispatchProps, IAppState> {
-  public state = {
-    Element: Spinner,
-    hasErrored: false,
-  };
-
-  private refHandler = {
-      toaster: setToast,
-  };
-
-  public componentWillMount() {
-    this.checkWhichComponent(this.props.isLoggedIn);
-  }
-
-  public componentWillReceiveProps(nextProps: IAppStoreProps & IAppDispatchProps) {
-    if (this.props.isLoggedIn !== nextProps.isLoggedIn) {
-      this.checkWhichComponent(nextProps.isLoggedIn);
+export class App extends React.PureComponent {
+    public render() {
+        return (
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/welcome/:to/:from" component={SplashPage} />
+                    <Route component={AtlasP} />
+                </Switch>
+            </BrowserRouter>
+        )
     }
-  }
-
-  public componentDidCatch(error: any, info: any) {
-    console.error(error, info);
-    this.setState({ hasErrored: true });
-  }
-
-  public render() {
-    if (this.state.hasErrored) {
-      this.handleErroredOut();
-    }
-
-    const { Element } = this.state;
-    return (
-      <div className="prevent-movement">
-        <Element />
-        <Toaster ref={this.refHandler.toaster} />
-      </div>
-    );
-  }
-
-  private async checkWhichComponent(isLoggedIn: boolean) {
-    if (!isLoggedIn) {
-      const loginElement = await import("./Components/Login/Login");
-      this.setState({ Element: loginElement.LoginComponent });
-      return;
-    }
-    const mainElement = await import("./Components/Main");
-    this.setState({ Element: mainElement.Main });
-  }
-
-  private handleErroredOut() {
-    this.props.emptyCache();
-    return <div className="centered">Rats, look like you found a bug. We've emptied the cache - try refreshing the page?</div>
-  }
 }
-
-function mapStateToProps(state: IStoreState): IAppStoreProps {
-  return {
-    fetching: state.DatabaseReducer.isFetching,
-    isLoggedIn: state.DatabaseReducer.isLoggedIn,
-  };
-}
-
-function mapDispatchToProps(dispatch: Dispatch): IAppDispatchProps {
-  return bindActionCreators({ emptyCache: EmptyDatabaseCache.create }, dispatch)
-}
-
-export const App = connect(mapStateToProps, mapDispatchToProps)(PureApp);
