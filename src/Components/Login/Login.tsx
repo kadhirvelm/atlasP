7660e0ee44d1f3c830a564ecb5fdccba4c30a95e
Kadhir <kadhirvelm@gmail.com>
2018-10-01 21:25:42 -0700

    Implement new login design (#76)

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -14,12 +14,14 @@ export interface ILoginComponentDispatchProps {
 }
 
 export interface ILoginComponentState {
+    claimNumber: string;
     password: string;
     phoneNumber: string;
 }
 
 class PureLoginComponent extends React.Component<ILoginComponentDispatchProps, ILoginComponentState> {
     public state: ILoginComponentState = {
+        claimNumber: "",
         password: "",
         phoneNumber: "",
     };
@@ -27,34 +29,39 @@ class PureLoginComponent extends React.Component<ILoginComponentDispatchProps, I
     public render() {
         return (
             <div className="centered fade-in">
-                <h2>Welcome to AtlasP</h2>
-                <div className="center-login">
-                    <div className="details">
+                <div className="login-title">Welcome to AtlasP</div>
+                <div className="login-center">
+                    <div className="login-main-container">
                         <EditableText
-                            className="textfield"
-                            onConfirm={this.storePhoneNumber}
-                            placeholder="Phone number"
+                            className="login-textfield"
+                            onChange={this.storeClaimNumber}
+                            placeholder="Phone number…"
                         />
-                        <PasswordField
-                            className="textfield"
-                            onChange={this.storePassword}
-                            placeHolder="Password"
-                        />
-                    </div>
-                    <div className="action-buttons">
                         <Button
-                            className="action-button"
+                            className="login-action-button"
                             disabled={this.checkClaim()}
                             id="claim-button"
-                            intent={Intent.NONE}
-                            onClick={this.handleClaim}
-                            text="Claim Account"
-                        />
-                        <div className="divider" />
-                        <Button
-                            className="action-button"
-                            disabled={this.checkSignIn()}
                             intent={Intent.PRIMARY}
+                            onClick={this.handleClaim}
+                            text="Claim"
+                        />
+                    </div>
+                    <div className="login-divider" />
+                    <div className="login-main-container">
+                        <EditableText
+                            className="login-textfield"
+                            onChange={this.storePhoneNumber}
+                            placeholder="Phone number…"
+                        />
+                        <PasswordField
+                            className="login-textfield"
+                            onChange={this.storePassword}
+                            placeHolder="Password…"
+                        />
+                        <Button
+                            className="login-action-button"
+                            disabled={this.checkSignIn()}
+                            intent={Intent.NONE}
                             id="authorize-button"
                             onClick={this.handleLogin}
                             text="Sign In"
@@ -65,19 +72,20 @@ class PureLoginComponent extends React.Component<ILoginComponentDispatchProps, I
         )
     }
 
+    private checkClaim() {
+        return this.state.claimNumber === "";
+    }
+
     private checkSignIn() {
         return this.state.phoneNumber === "" || this.state.password === "";
     }
 
-    private checkClaim() {
-        return this.state.phoneNumber === "";
-    }
-
+    private storeClaimNumber = (claimNumber: string) => this.setState({ claimNumber })
     private storePhoneNumber = (phoneNumber: string) => this.setState({ phoneNumber })
     private storePassword = (password: string) => this.setState({ password });
 
+    private handleClaim = () => this.props.claim(this.state.claimNumber);
     private handleLogin = () => this.props.login(this.state.phoneNumber, this.state.password);
-    private handleClaim = () => this.props.claim(this.state.phoneNumber);
 }
 
 function mapDispatchToProps(dispatch: Dispatch) {
