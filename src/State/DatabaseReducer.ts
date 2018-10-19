d61f0bdeb6905ffbe94644bd4210a433fabf573d
Kadhir <kadhirvelm@gmail.com>
2018-10-18 23:18:36 -0700

    Appending new user and new event connections (#98)

--- src/State/DatabaseReducer.ts
+++ src/State/DatabaseReducer.ts
@@ -34,12 +34,22 @@ export const DatabaseReducer = TypedReducer.builder<IStoreState["DatabaseReducer
         });
     })
     .withHandler(UpdateUserData.TYPE, (state, payload) => {
+        const currentUser = state.currentUser;
+        if (currentUser !== undefined && currentUser.connections !== undefined) {
+            currentUser.connections[payload.id] = [];
+        }
         return setWith(state, {
+            currentUser,
             userData: { ...state.userData, [payload.id]: payload },
         });
     })
     .withHandler(UpdateEventData.TYPE, (state, payload) => {
+        const currentUser = state.currentUser;
+        if (currentUser !== undefined && currentUser.connections !== undefined) {
+            payload.attendees.forEach((user) => (currentUser.connections as any)[user.id].push(payload.id));
+        }
         return setWith(state, {
+            currentUser,
             eventData: { ...state.eventData, [payload.id]: payload },
         });
     })
